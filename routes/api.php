5faed47ae8ab09efd73cee8f743eff0b7361022c
Steve Harmeyer <harmeyersteve@gmail.com>
2020-06-16 09:14:52 -0700

    added api middleware

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'prefix' => 'api/v1/error',
-    'middleware' => 'auth:api',
+    'middleware' => [
+        'api',
+        'auth:api',
+    ],
     'namespace' => 'Submtd\LaravelApiError\Controllers',
 ], function () {
     Route::get('/', 'ListErrors')->name('api-error.list');
